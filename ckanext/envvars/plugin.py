2773c0e3dc42e37ff733218ca8fdcbed074887ac
Brook Elgie <specialbrew@gmail.com>
2015-07-21 15:30:32 +0100

    Update config with values from env vars

--- ckanext/envvars/plugin.py
+++ ckanext/envvars/plugin.py
@@ -1,5 +1,6 @@
+import os
+
 import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
 
 import logging
 log = logging.getLogger(__name__)
@@ -8,7 +9,29 @@ log = logging.getLogger(__name__)
 class EnvvarsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
+    @staticmethod
+    def _envvar_to_ini(key):
+        '''Transforms an env var formatted key to ini formatting.'''
+        # If the key starts with 'CKAN___' (3 underscores).
+        if key.startswith('CKAN___'):
+            key = key[7:]
+        # Set it to lowercase
+        key = key.lower()
+        # Replace dunders with dots
+        key = key.replace('__', '.')
+
+        return key
+
     # IConfigurer
 
     def update_config(self, config):
-        log.info('updated config')
+
+        # get all vars beginning with 'CKAN'
+        ckan_vars = ((k, v) for k, v in os.environ.items()
+                     if k.startswith('CKAN'))
+
+        # transform vars into ini settings format
+        ckan_vars = ((self._envvar_to_ini(k), v) for k, v in ckan_vars)
+
+        # override config settings with new values
+        config.update(dict(ckan_vars))
