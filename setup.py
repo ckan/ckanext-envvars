c1dadd9d80a052ef5346c92b7b246e8a4e901a98
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-07-12 09:55:19 -0400

    docs: update setup.py
    Remove Python 2.6 support and require ckantoolkit install

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ],
@@ -59,7 +58,9 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=[],
+    install_requires=[
+        'ckantoolkit',
+    ],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
